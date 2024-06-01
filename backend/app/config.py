630733cfe4b4b0f2259aaa9198101a87223861ea
aaron (kb) <aaron.sanders342@gmail.com>
2024-06-01 01:52:20 -0500

    base config working, next is dev

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,9 @@
 import os
 
+POSTGRES_USER = os.getenv("POSTGRES_USER", "username")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "db")
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://yourusername:yourpassword@db/moviedb')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_DB}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
