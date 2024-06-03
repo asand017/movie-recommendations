4ad33178e3b057693817b1b9d777e336246dbf23
aaron (kb) <aaron.sanders342@gmail.com>
2024-06-03 00:40:30 -0500

    add todo to source additional movie datasets

--- backend/data/collect_data.py
+++ backend/data/collect_data.py
@@ -3,7 +3,9 @@ import pandas as pd
 def ingest_movies(tsv_file, sample_size=1000):
     df = pd.read_table(tsv_file)
     movies = df[df['titleType'] == 'movie']
-    print(movies.head(15))
+    print(movies.head(15)) 
+    # we can take [primaryTitle -> Movie.title, genres -> genre (comma separated string), year -> startYear]
+    # TODO: description is not in the imdb dataset, so we need to find another data source for movie plot descriptions
 
 if __name__ == '__main__':
     movie_titles_tsv = '/Volumes/Malenia/Data/title.basics.tsv'
